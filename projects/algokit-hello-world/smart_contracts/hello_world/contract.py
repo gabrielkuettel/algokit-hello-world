ab1e0fc5f93be2f84c707b8d837a06c0616fc9b3
Gabriel Kuettel <gabrielkuettel@gmail.com>
2024-09-18 04:34:45 -0600

    feat: store greeting in box storage

--- projects/algokit-hello-world/smart_contracts/hello_world/contract.py
+++ projects/algokit-hello-world/smart_contracts/hello_world/contract.py
@@ -1,8 +1,16 @@
-from algopy import ARC4Contract, String
-from algopy.arc4 import abimethod
-
+from algopy import ARC4Contract, Box, arc4, subroutine
 
 class HelloWorld(ARC4Contract):
-    @abimethod()
-    def hello(self, name: String) -> String:
-        return "Hello, " + name
+    def __init__(self) -> None:
+        self.box_greeting = Box(arc4.String, key='greeting')
+
+    @arc4.abimethod()
+    def hello(self, name: arc4.String) -> arc4.String:
+        assert name != "", "Name cannot be empty"
+        greeting = "Hello, " + name
+        self.store_greeting(greeting)
+        return greeting
+    
+    @subroutine
+    def store_greeting(self, greeting: arc4.String) -> None:
+        self.box_greeting.value = greeting
